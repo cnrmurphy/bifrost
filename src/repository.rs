0f75006a0174b370af55a7f448c7b56673e7a2b1
cnrmurphy <conor.murphy4304@gmail.com>
2024-06-28 19:48:12 -0400

    add mongo repo

--- src/repository.rs
+++ src/repository.rs
@@ -1,8 +1,10 @@
-use crate::models::Order;
+use mongodb::{bson::doc, options::FindOptions};
+
+use crate::{db::DB, models::Order};
 use std::sync::{Arc, Mutex};
 
 pub trait OrderRepo: Send + Sync {
-    fn add_order(&self, order: &Order);
+    fn insert_order(&self, order: &Order);
     fn get_orders(&self) -> Result<Vec<Order>, Box<dyn std::error::Error>>;
 }
 
@@ -23,7 +25,7 @@ impl InMemoryOrderRepo {
 }
 
 impl OrderRepo for InMemoryOrderRepo {
-    fn add_order(&self, order: &Order) {
+    fn insert_order(&self, order: &Order) {
         let mut orders = self.orders.lock().unwrap();
         orders.push(order.clone());
     }
@@ -31,5 +33,37 @@ impl OrderRepo for InMemoryOrderRepo {
     fn get_orders(&self) -> Result<Vec<Order>, Box<dyn std::error::Error>> {
         let orders = self.orders.lock().unwrap();
         Ok(orders.clone())
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct MongoOrderRepo {
+    collection: mongodb::Collection<Order>,
+}
+
+impl MongoOrderRepo {
+    pub fn new(db: &DB) -> Self {
+        let collection = db.client.collection("orders");
+        Self { collection }
+    }
+
+    pub async fn insert_order(
+        &self,
+        order: &Order,
+    ) -> Result<mongodb::results::InsertOneResult, mongodb::error::Error> {
+        let result = self.collection.insert_one(order, None).await;
+        return result;
+    }
+
+    pub async fn fetch_orders(&self) -> Result<Vec<Order>, mongodb::error::Error> {
+        let mut orders: Vec<Order> = Vec::new();
+        let options = FindOptions::builder().limit(100).build();
+        let mut cursor = self.collection.find(None, options).await?;
+        while cursor.advance().await? {
+            let order = cursor.deserialize_current()?;
+            orders.push(order);
+        }
+
+        Ok(orders)
     }
 }
