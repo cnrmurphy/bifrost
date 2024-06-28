c26b3ac8b81ec416a600cd71ac522f38993f17d6
cnrmurphy <conor.murphy4304@gmail.com>
2024-06-28 19:46:55 -0400

    add mongo repo to axum app state

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
+mod db;
 mod models;
 mod repository;
-
-use std::sync::Arc;
 
 use askama::Template;
 use axum::{
@@ -11,8 +10,10 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
-use models::Order;
-use repository::{InMemoryOrderRepo, OrderRepo};
+use dotenv::dotenv;
+use models::{Order, OrderRequest};
+use repository::{InMemoryOrderRepo, MongoOrderRepo, OrderRepo};
+use std::sync::Arc;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[derive(Template)]
@@ -21,7 +22,7 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
     <h1>Portfolio</h1>
     <ul>
     {% for order in orders %}
-        <li>{{ order.symbol }} - {{ order.side }} - {{ order.kind }} - {{ order.quantity }}</li>
+        <li>{{ order.symbol }} - {{ order.side }} - {{ order.kind }} - {{ order.quantity }} - {% if order.status.is_some() %} {{ order.status.as_ref().unwrap() }} {% endif %}</li>
     {% endfor %}
     <ul>
     "#,
@@ -34,10 +35,12 @@ struct OrdersTemplate<'a> {
 #[derive(Clone)]
 struct AppState {
     order_repo: Arc<dyn OrderRepo>,
+    mongo_order_repo: MongoOrderRepo,
 }
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
@@ -46,14 +49,20 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
+    let db = db::DB::init().await.unwrap();
+
     let order_repo = InMemoryOrderRepo::new_with_arc();
+    let mongo_order_repo = MongoOrderRepo::new(&db);
 
     let app = Router::new()
         .route("/", get(root))
         .route("/portfolio", get(render_portfolio))
         .route("/orders/limit", post(place_limit_order))
         .route("/orders/market", post(place_market_order))
-        .with_state(AppState { order_repo });
+        .with_state(AppState {
+            order_repo,
+            mongo_order_repo,
+        });
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     tracing::info!("Bifrost listening on {}", listener.local_addr().unwrap());
@@ -64,24 +73,52 @@ async fn root() -> &'static str {
     "Hello world!"
 }
 
-async fn render_portfolio(State(state): State<AppState>) -> Html<String> {
-    let orders = state.order_repo.get_orders().unwrap();
+async fn render_portfolio(State(state): State<AppState>) -> Result<Html<String>, StatusCode> {
+    let orders = state
+        .mongo_order_repo
+        .fetch_orders()
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
     let tmpl = OrdersTemplate { orders: &orders };
-    Html(tmpl.render().unwrap())
+    tmpl.render()
+        .map(Html)
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
 }
 
-async fn place_limit_order(State(state): State<AppState>, Json(input): Json<Order>) -> StatusCode {
+async fn place_limit_order(
+    State(state): State<AppState>,
+    Json(input): Json<OrderRequest>,
+) -> StatusCode {
     tracing::debug!("limit order request");
     tracing::debug!("{:?}", input);
-    state.order_repo.add_order(&input);
-
-    StatusCode::OK
+    let result = state.mongo_order_repo.insert_order(&input.into()).await;
+    match result {
+        Ok(result) => {
+            tracing::debug!("{:?}", result);
+            StatusCode::OK
+        }
+        Err(error) => {
+            tracing::error!("{:?}", error);
+            StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
 }
 
-async fn place_market_order(State(state): State<AppState>, Json(input): Json<Order>) -> StatusCode {
+async fn place_market_order(
+    State(state): State<AppState>,
+    Json(input): Json<OrderRequest>,
+) -> StatusCode {
     tracing::debug!("market order request");
     tracing::debug!("{:?}", input);
-    state.order_repo.add_order(&input);
-
-    StatusCode::OK
+    let result = state.mongo_order_repo.insert_order(&input.into()).await;
+    match result {
+        Ok(result) => {
+            tracing::debug!("{:?}", result);
+            StatusCode::OK
+        }
+        Err(error) => {
+            tracing::error!("{:?}", error);
+            StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
 }
