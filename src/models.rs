e216302d1a18ebc2f150bf2f35241878a2ea03e4
cnrmurphy <conor.murphy4304@gmail.com>
2024-06-28 19:47:42 -0400

    add OrderRequest model

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,7 @@
+use mongodb::bson::oid::{self, ObjectId};
 use strum_macros::EnumString;
 
-#[derive(EnumString, serde::Deserialize, Debug, Clone)]
+#[derive(EnumString, serde::Deserialize, serde::Serialize, Debug, Clone)]
 pub enum OrderSide {
     Buy,
     Sell,
@@ -16,7 +17,7 @@ impl std::fmt::Display for OrderSide {
     }
 }
 
-#[derive(EnumString, serde::Deserialize, Debug, Clone)]
+#[derive(EnumString, serde::Deserialize, serde::Serialize, Debug, Clone)]
 pub enum OrderKind {
     Market,
     Limit,
@@ -32,19 +33,64 @@ impl std::fmt::Display for OrderKind {
     }
 }
 
-#[derive(EnumString, serde::Deserialize, Debug, Clone)]
+#[derive(EnumString, serde::Deserialize, serde::Serialize, Debug, Clone)]
 pub enum OrderId {
     Int(i64),
 }
 
-#[derive(serde::Deserialize, Debug, Clone)]
+#[derive(EnumString, serde::Deserialize, serde::Serialize, Debug, Clone)]
+pub enum OrderStatus {
+    Open,
+    Filled,
+    Cancelled,
+}
+
+impl std::fmt::Display for OrderStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        tracing::debug!("writing order kind");
+        match self {
+            OrderStatus::Open => write!(f, "Open"),
+            OrderStatus::Filled => write!(f, "Filled"),
+            OrderStatus::Cancelled => write!(f, "Cancelled"),
+        }
+    }
+}
+
+#[allow(non_snake_case)]
+#[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
 pub struct Order {
-    pub id: u64,
+    #[serde(rename = "_id")]
+    pub id: ObjectId,
     pub symbol: String,
     pub quantity: String,
     pub price: String,
-    pub timestamp: u64,
+    pub side: OrderSide,
+    pub kind: OrderKind,
+    pub status: Option<OrderStatus>,
+    pub ttl: Option<u64>,
+}
+
+#[derive(serde::Deserialize, Debug, Clone)]
+pub struct OrderRequest {
+    pub symbol: String,
+    pub quantity: String,
+    pub price: String,
     pub side: OrderSide,
     pub kind: OrderKind,
     pub ttl: Option<u64>,
+}
+
+impl Into<Order> for OrderRequest {
+    fn into(self) -> Order {
+        Order {
+            id: oid::ObjectId::new(),
+            symbol: self.symbol,
+            quantity: self.quantity,
+            price: self.price,
+            side: self.side,
+            kind: self.kind,
+            status: Some(OrderStatus::Open),
+            ttl: None,
+        }
+    }
 }
